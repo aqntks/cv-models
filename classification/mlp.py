2b50d85d014720329e143e4bbc44bedbfc5bff38
aqntks <cjd0030@gmail.com>
2021-08-21 19:55:25 +0900

    MLP(Multi Layer Perceptron) 추가

---
+++ classification/mlp.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(32 * 32 * 3, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 32 * 32 * 3)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        x = F.log_softmax(x, dim=1)
+        return x
