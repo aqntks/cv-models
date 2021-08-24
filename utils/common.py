9f32db5b34ca1eae3caadf1407f02a456f2551f3
aqntks <42826598+aqntks@users.noreply.github.com>
2021-08-24 10:48:13 +0900

    학습 pt 파일 저장 경로 변경

--- utils/common.py
+++ utils/common.py
@@ -3,6 +3,6 @@ import torch
 
 def model_save(model, state_dict=False):
     if state_dict:
-        torch.save(model.state_dict(), 'model.pth')
+        torch.save(model.state_dict(), 'result/model.pt')
     else:
-        torch.save(model, 'model.pth')
+        torch.save(model, 'result/model.pt')
